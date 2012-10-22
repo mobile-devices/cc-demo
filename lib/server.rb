e36157411dd6161c8e683f1c28be59a010e015f3
Alexandre Mora <alexandre.mora@mobile-devices.fr>
2012-10-22 12:56:02 +0200

    Update post message request.

--- lib/server.rb
+++ lib/server.rb
@@ -3,8 +3,10 @@
 require 'sinatra/base'
 require 'coffee-script'
 require 'execjs'
-
 require 'sprockets'
+require 'json'
+require 'net/http'
+require 'uri'
 
 class Server < Sinatra::Base
   get '/' do
@@ -16,6 +18,36 @@ class Server < Sinatra::Base
     env = Sprockets::Environment.new
     env.append_path "lib/views/coffee"
     env["app.coffee"]
+  end
+
+  post '/message' do
+    # extract data from the post ajax post request
+    to_send, payload, connect = {}, {}, {}
+    params.each do |key, value|
+      if key != 'token' && key != 'url'
+        payload[key] = value
+      else
+        connect[key] = value
+      end
+    end
+    # struct of a post request (see documentation)
+    to_send['message'] = payload
+    uri = URI(connect['url'])
+    path = uri.path.split('.')
+    begin
+      # post a new message to APIv3
+      net = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(path[0])
+      request.content_type = 'application/json'
+      request.body = to_send.to_json
+      request.basic_auth connect['token'], 'X'
+      response = net.start do |http|
+        http.request(request)
+      end
+    return to_send.to_json
+    rescue SocketError => se
+      "Got socket error: #{se}"
+    end
   end
 
   # Usage: partial :foo
--- lib/views/coffee/post.js.coffee
+++ lib/views/coffee/post.js.coffee
@@ -3,22 +3,22 @@
 App.postMessageFormView = Em.View.extend({
     tagName: 'form',
     # fields
-    recipient: '',
+    asset: '',
     channel: '',
     payload: '',
     # action on form submit
     submit: (e) ->
       # data from the form
-      post_data = 'recipient=' + this.get('recipient') + '&channel=' + this.get('channel') + '&b64_payload=' + this.get('payload')
+      post_data = 'url=' + App.baseUrl('messages') + '&token=' + App.token() + '&recipient=' + this.get('asset') + '&asset=' + this.get('asset') + '&channel=' + this.get('channel') + '&b64_payload=' + this.get('payload')
       $.ajax({
-        # POST request (see documentation)
-        url: App.baseUrl('messages'),
+        # POST request (see server.rb /message)
+        url: '/message',
         type: 'POST',
         data: post_data,
-        dataType: 'jsonp',
-        # display "Message sended!" on success
+        contentType: 'application/x-www-form-urlencoded',
+        # display "Message sent!" on success
         success: (msg) ->
-         alert("Message sended!");
+         alert("Message sent!");
         # display the "error status" on error
         error: (xhr, ajaxOptions, thrownError) ->
           alert(xhr.status);
